987bf48cbeb4df75e1b08157445a0f6176145db8
Rob Rix <rob.rix@github.com>
2014-11-03 19:11:49 +0000

    Initial Commit

---
+++ CCVal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CCVal
+//
+//  Created by Rob Rix on 2014-11-03.
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
